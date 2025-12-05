59b2134c4a92cd4aede94c82f1d3f11cb77a1017
Sashank <sashanka.samantray@rabobank.nl>
2025-12-05 21:44:08 +0100

    stateless gatherer section completed

--- src/test/java/com/java/functional/programming/streamgatherers/stateless/StreamIntegratorWorkingWithDownstreamTest.java
+++ src/test/java/com/java/functional/programming/streamgatherers/stateless/StreamIntegratorWorkingWithDownstreamTest.java
@@ -1,4 +1,4 @@
-package com.java.functional.programming.streamgatherers;
+package com.java.functional.programming.streamgatherers.stateless;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
