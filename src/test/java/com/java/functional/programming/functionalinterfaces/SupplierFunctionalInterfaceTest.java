07037079ff42f6ab6db77983f829e09000f71c9c
Sashank <sashanka.samantray@rabobank.nl>
2025-12-07 14:03:38 +0100

    supplier document updated

--- src/test/java/com/java/functional/programming/functionalinterfaces/SupplierFunctionalInterfaceTest.java
+++ src/test/java/com/java/functional/programming/functionalinterfaces/SupplierFunctionalInterfaceTest.java
@@ -8,16 +8,18 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.function.Supplier;
 
-// The Supplier Interface is a part of the java.util.function package which has been introduced since Java 8, to implement functional programming in Java.
-// It represents a function which does not take in any argument but produces a value of type T.
-// The lambda expression assigned to an object of Supplier type is used to define its get() which eventually produces a value.
-// Suppliers are useful when we don’t need to supply any value and obtain a result at the same time.
+/**
+ * The Supplier Interface is a part of the java.util.function package which has been introduced since Java 8, to implement functional programming in Java.
+ * It represents a function which does not take in any argument but produces a value of type T.
+ * The lambda expression assigned to an object of Supplier type is used to define its get() which eventually produces a value.
+ * Suppliers are useful when we don’t need to supply any value and obtain a result at the same time.
+ * */
 @Slf4j
 public class SupplierFunctionalInterfaceTest {
 
     @Test
     public void supplier_simple_example_test() {
-        Supplier<List<Student>> studentSupplier = () -> StudentDatabase.getAllStudents();
-        System.out.println(studentSupplier.get());
+        Supplier<List<Student>> studentSupplier = StudentDatabase::getAllStudents;
+        log.info("All students :{}", studentSupplier.get());
     }
 }
